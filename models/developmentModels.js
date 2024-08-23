1f0b0f85ad52f7ef263e94c2aad844e71238c51d
Ahmed <ahmedkhan895.ak@gmail.com>
2024-08-23 17:12:26 +0100

    changed email send function to accept username and email address, and configured email body

--- models/developmentModels.js
+++ models/developmentModels.js
@@ -1,15 +1,12 @@
-// models/developmentModels.js
 const mongoose = require("mongoose");
 const developmentSchema = require("./development");
 
-// Model for the existing "developments" collection
 const Development = mongoose.model(
   "Development",
   developmentSchema,
   "developments"
 );
 
-// Model for the new "deletedDevelopments" collection
 const DeletedDevelopment = mongoose.model(
   "DeletedDevelopment",
   developmentSchema,
