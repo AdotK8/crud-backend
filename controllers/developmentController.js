98ebd45a17460365055d5f1269c814fdb1748eba
Ahmed <ahmedkhan895.ak@gmail.com>
2024-10-14 14:53:02 +0100

    console log for testing

--- controllers/developmentController.js
+++ controllers/developmentController.js
@@ -178,7 +178,7 @@ exports.sendMatchEmail = async (req, res) => {
   const { selection, email, name } = req.body;
 
   try {
-    console.log(process.env.PERSONAL_EMAIL);
+    console.log("BCC Email:", process.env.BCC_EMAIL);
     const transporter = nodemailer.createTransport({
       service: "gmail",
       host: "smtp.gmail.com",
